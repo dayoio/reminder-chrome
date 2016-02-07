852bb2aebd523fc9b1b22d65384bb2de23624da9
Dayo Zang <minoscc@gmail.com>
2016-02-07 22:15:23 +0800

    add ngStorage

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,27 +4,37 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.alarms.onAlarm.addEventListener(alarm => {
-  if(alarm && alarm.name.indexOf('reminder_') === -1)
-  {
-    //
-    chrome.notifications.create('title', {
-      title: '',
-      message: '',
-      iconUrl: ''
+// Alarm handler
+chrome.alarms.onAlarm.addListener(alarm => {
+  if (alarm && alarm.name.indexOf('reminder_') === -1) {
+    // find remind detail
+    let r = {};
+    chrome.notifications.create('Reminder', {
+      title: 'It\'s time to do this.',
+      message: r.message,
+      iconUrl: 'images/icon-64.png'
     });
-
   }
 });
 
-chrome.runtime.onMessage.addEventListener(message => {
-  //
-  if(message.type === 'put' )
+// Message Handler
+chrome.runtime.onMessage.addListener(message => {
+  if(message.remind !== undefined)
   {
-    chrome.alarms.create('', {});
-  }
-  else if(message.type === 'delete')
-  {
-    chrome.alarms.clear('');
+    if(message.remind.enable)
+    {
+      chrome.alarms.create(message.remind.name, {
+        'periodInMinutes': message.remind.after
+      });
+    }else{
+      chrome.alarms.clear(message.remind.name, function (val) {
+        //
+        if(val) {
+          // success
+        }else{
+          // fail
+        }
+      });
+    }
   }
 });
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,77 +1,105 @@
 'use strict';
 
-const APP_NAME = 'Reminder';
-
+// reminder app
 angular
-  .module('app', ['ngMaterial'])
-    .config(function ($mdThemingProvider, $mdIconProvider) {
-      $mdThemingProvider.theme('default')
-        .primaryPalette('teal')
-        .accentPalette('deep-orange');
-
-      $mdIconProvider
-        .icon('alarm', '../icons/ic_alarm_white_18px.svg', 18)
-        .icon('alarm_add', '../icons/ic_add_alarm_white_18px.svg', 18)
-        .icon('delete', '../icons/ic_delete_black_18px.svg', 18)
-        .icon('help', '../icons/ic_help_black_18px.svg', 18);
-    })
+  .module('app', ['ngMaterial', 'ngStorage'])
+  .config(function ($mdThemingProvider, $mdIconProvider) {
+    // set theme
+    $mdThemingProvider.theme('default')
+      .primaryPalette('teal')
+      .accentPalette('deep-orange');
+    // icons
+    $mdIconProvider
+      .icon('alarm', '../icons/ic_alarm_white_18px.svg', 18)
+      .icon('alarm_add', '../icons/ic_add_alarm_white_18px.svg', 18)
+      .icon('delete', '../icons/ic_delete_black_18px.svg', 18)
+      .icon('help', '../icons/ic_help_black_18px.svg', 18);
+  })
   // Dialog controller
-  .controller('putController', function($scope, $mdDialog, current){
-    $scope.current = current;
-    $scope.hide = function () {
-      $mdDialog.hide();
-    };
+  .controller('dialogController', function ($scope, $mdDialog, current) {
+    $scope.current = current == null ? {
+      enable: false,
+      repeat: false
+    } : current;
+    // handlers
     $scope.cancel = function () {
       $mdDialog.cancel();
     };
+    $scope.delete = function () {
+      $scope.deleteRemind($scope.current);
+      $mdDialog.hide();
+    };
     $scope.ok = function () {
-      //
       $mdDialog.hide($scope.current);
     };
   })
-  // Toolbar controller
-  .controller('mainController', function ($scope, $rootScope, $mdDialog) {
-    // add & edit form.
-    $scope.title = APP_NAME;
-    $scope.current = null;
+  // Main controller
+  .controller('mainController', function ($scope, $rootScope, $localStorage, $mdDialog, $log) {
 
-    $rootScope.onClickRemind = function($event, remind = null){
-      // show dialog
-      $mdDialog.show({
-        controller: 'putController',
-        templateUrl: 'remind.tmpl.html',
-        parent: angular.element(document.body),
-        targetEvent: $event,
-        clickOutsideToClose: true,
-        locals: { current: remind }
-      })
-      .then(function(){
-        // ok
-      }, function (){
-        // cancel
-      });
-
+    $rootScope.i18n = function (key) {
+      return chrome.i18n.getMessage(key);
     };
-  })
-  // Reminds controller
-  .controller('listController', function ($scope) {
-    // fetch data
 
-    // list...
-    $scope.reminds = [];
-    for(let i = 0; i < 10; i++) {
-      $scope.reminds.push(
+    $rootScope.$storage = $localStorage.$default({
+      reminds: []
+    });
+
+    $rootScope.deleteRemind = function (remind) {
+      if(remind.name === undefined){
+        return;
+      }
+      let res = $rootScope.getRemind(remind);
+      if(res.index > -1)
+      {
+        if(res.value.enable)
         {
-          message: 'hello world',
-          repeat: true,
-          after: 30,
-          next: '2016 02 05 23:44',
-          enable: false
+          chrome.runtime.sendMessage({
+            remind: res.value
+          });
         }
-      );
+        $rootScope.$storage.reminds.splice(res.index, 1);
+      }
+    };
+
+    $rootScope.getRemind = function (remind) {
+      try{
+        for(let i =0;i< $rootScope.$storage.reminds.length; i++)
+        {
+          if($rootScope.$storage.reminds[i].name === remind.name){
+            return {
+              index: i,
+              value: $rootScope.$storage.reminds[i]
+            };
+          }
+        }
+      }catch(err){
+        $log.error(err);
+      }
+      return {
+        index: -1
+      };
     }
 
-    $scope.onSwipeRight = function () {
-      console.log('swipe');
+    $rootScope.putRemind = function ($event, remind = null) {
+      // show dialog
+      $mdDialog.show({
+          controller: 'dialogController',
+          templateUrl: 'remind.tmpl.html',
+          parent: angular.element(document.body),
+          targetEvent: $event,
+          clickOutsideToClose: true,
+          locals: {current: angular.copy(remind)}
+        })
+        .then(function (newRemind) {
+          let res = $rootScope.getRemind(newRemind);
+          if(res.index > -1){
+            $rootScope.$storage.reminds.splice(res.index, 1, newRemind);
+            chrome.runtime.sendMessage({
+              remind: newRemind
+            });
+          }else{
+            $rootScope.$storage.reminds.push(newRemind);
+          }
+        });
     };
   });
